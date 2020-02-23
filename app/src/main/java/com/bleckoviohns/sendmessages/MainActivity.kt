554031141e2f5b9d5e0cf2021124bc12b9fb5895
JohnsVargas <juan10cv@gmail.com>
2020-02-22 21:59:46 -0600

    update commit

--- app/src/main/java/com/bleckoviohns/sendmessages/MainActivity.kt
+++ app/src/main/java/com/bleckoviohns/sendmessages/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.bleckoviohns.sendmessages
 
 import android.Manifest
-import android.R.id.message
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.telephony.SmsManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
+import java.text.DecimalFormat
 
 
 var ACTION_SMS_SENT = "ACTION_SMS_SENT"
 var ACTION_SMS_DELIVERED = "ACTION_SMS_DELIVERED"
 var MY_PERMISSIONS_REQUEST_SEND_SMS = 0
+var longitudAnterior:Double = 0.0
+var latitudAnterior:Double = 0.0
+var isFirsTime = true
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,18 +47,66 @@ class MainActivity : AppCompatActivity() {
                     MY_PERMISSIONS_REQUEST_SEND_SMS)
             }
         }
+        getLocation()
+    }
+    @SuppressLint("MissingPermission")
+    fun getLocation(){
+        ActivityCompat.requestPermissions(this,
+            arrayOf( Manifest.permission.ACCESS_FINE_LOCATION),1)
+        if(Check_FINE_LOCATION(this)){
+            // Acquire a reference to the system Location Manager
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            // Define a listener that responds to location updates
+            val locationListener = object : LocationListener {
+
+                override fun onLocationChanged(location: Location) {
+                    // Called when a new location is found by the network location provider.
+                    if(isFirsTime){
+                        longitudAnterior = location.longitude
+                        latitudAnterior = location.latitude
+                        isFirsTime = false
+                    }
+                    txt_altitud.text = location.latitude.toString()
+                    txt_longitud.text = location.longitude.toString()
+                    Log.d("Location","($latitudAnterior,$longitudAnterior)-- (${location.latitude},${location.longitude})")
+                    val metro:FloatArray = FloatArray(4)
+                    Location.distanceBetween(latitudAnterior, longitudAnterior,location.latitude,location.longitude,metro)
+
+                    txt_metros.text = String.format("${metro[0]} metros")
+                    if(metro[0]>1.0){
+                        sendSms(metro[0])
+                    }
+                    longitudAnterior = location.longitude
+                    latitudAnterior = location.latitude
+
+                }
+
+                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                }
+
+                override fun onProviderEnabled(provider: String) {
+                }
+
+                override fun onProviderDisabled(provider: String) {
+                }
+            }
+
+            // Register the listener with the Location Manager to receive location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.98f,locationListener)
+        }
 
     }
-    fun sendSms(){
-        var sm: SmsManager  = SmsManager.getDefault()
-        var parts : ArrayList<String> = sm.divideMessage("El celular se movio")
-        var iSent: Intent = Intent(ACTION_SMS_SENT)
-        var piSent: PendingIntent = PendingIntent.getBroadcast(this, 0, iSent, 0)
+
+    fun sendSms(metros:Float){
+        val sm: SmsManager  = SmsManager.getDefault()
+        val parts : ArrayList<String> = sm.divideMessage("El celular se movio $metros metros")
+        val iSent: Intent = Intent(ACTION_SMS_SENT)
+        val piSent: PendingIntent = PendingIntent.getBroadcast(this, 0, iSent, 0)
         val iDel = Intent(ACTION_SMS_DELIVERED)
         val piDel = PendingIntent.getBroadcast(this, 0, iDel, 0)
 
         if (parts.size == 1) {
-            var msg = parts[0]
+            val msg = parts[0]
             sm.sendTextMessage("+525525188210", null, msg, piSent, piDel)
         } else {
             val sentPis = ArrayList<PendingIntent>()
@@ -67,7 +124,7 @@ class MainActivity : AppCompatActivity() {
         when (requestCode) {
             MY_PERMISSIONS_REQUEST_SEND_SMS -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    sendSms()
+                    sendSms(0.0f)
                     Toast.makeText(applicationContext, "SMS sent.", Toast.LENGTH_LONG).show()
                 } else {
                     Toast.makeText(applicationContext, "SMS faild, please try again.", Toast.LENGTH_LONG).show()
@@ -118,5 +175,11 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    fun Check_FINE_LOCATION(act: Activity): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(act, Manifest.permission.ACCESS_FINE_LOCATION)
+        return result == PackageManager.PERMISSION_GRANTED
     }
 }
